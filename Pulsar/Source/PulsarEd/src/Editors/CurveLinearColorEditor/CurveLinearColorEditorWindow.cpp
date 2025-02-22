487bcf6f53f6d76bc8ff55480285c44b3dd08930
JomiXedYu <JomiXedYu@outlook.com>
2025-02-22 13:22:56 +0800

    Update CurveLinearColorEditorWindow.cpp

--- Pulsar/Source/PulsarEd/src/Editors/CurveLinearColorEditor/CurveLinearColorEditorWindow.cpp
+++ Pulsar/Source/PulsarEd/src/Editors/CurveLinearColorEditor/CurveLinearColorEditorWindow.cpp
@@ -1,6 +1,5 @@
 #include "PulsarEd/Editors/CurveLinearColorEditor/CurveLinearColorEditorWindow.h"
 
-#include "ImCurveEdit.h"
 #include "ImGuiExt.h"
 #include "ImGradientHDR.h"
 #include "PropertyControls/PropertyControl.h"
@@ -19,72 +18,6 @@ namespace pulsared
         base::OnClose();
     }
 
-    struct RampEdit : ImCurveEdit::Delegate
-    {
-        CurveLinearColor* m_curve;
-        array_list<array_list<ImVec2>> m_imData;
-        ImVec2 m_min{};
-        ImVec2 m_max{};
-
-        RampEdit(CurveLinearColor* curve) : m_curve(curve)
-        {
-            BuildImData();
-        }
-
-        size_t GetCurveCount() override { return m_curve->GetCurveCount(); }
-        ImVec2& GetMin() override { return m_min; }
-        ImVec2& GetMax() override { return m_max; }
-        size_t GetPointCount(size_t curveIndex) override
-        {
-            return m_curve->GetCurveData(curveIndex)->GetKeyCount();
-        }
-
-        uint32_t GetCurveColor(size_t curveIndex) override
-        {
-            return 0xFFFFFFFF;
-        }
-
-        void BuildImData()
-        {
-            m_imData.resize(m_curve->GetCurveCount());
-            for (int curveIndex = 0; curveIndex < m_imData.size(); ++curveIndex)
-            {
-                auto& curve = m_curve->GetCurveData(curveIndex);
-                m_imData[curveIndex].resize(curve->GetKeyCount());
-                auto keyCount = curve->GetKeyCount();
-                for (int keyIndex = 0; keyIndex < keyCount; ++keyIndex)
-                {
-                    m_imData[curveIndex][keyIndex] = ImVec2( curve->GetKey(keyIndex).Time, curve->GetKey(keyIndex).Value );
-                }
-            }
-        }
-
-        ImVec2* GetPoints(size_t curveIndex) override
-        {
-            return m_imData[curveIndex].data();
-        }
-
-        int EditPoint(size_t curveIndex, int pointIndex, ImVec2 value) override
-        {
-            auto key = m_curve->GetCurveData(curveIndex)->GetKey(pointIndex);
-            key.Time = value.x;
-            key.Value = value.y;
-            m_curve->GetCurveData(curveIndex)->SetKey(pointIndex, key);
-            BuildImData();
-            return 0;
-        }
-
-        void AddPoint(size_t curveIndex, ImVec2 value) override
-        {
-            auto key = CurveKey{};
-            key.Time = value.x;
-            key.Value = value.y;
-            m_curve->GetCurveData(curveIndex)->AddKey(key);
-
-            BuildImData();
-        }
-    };
-
     void CurveLinearColorEditorWindow::OnDrawImGui(float dt)
     {
         base::OnDrawImGui(dt);
@@ -102,7 +35,7 @@ namespace pulsared
         for (int i = 0; i < keyCount; ++i)
         {
             auto time = m_colorCurve->GetCurveData(0)->GetKey(i).Time;
-            std::array<float, 3> color;
+            std::array<float, 3> color{};
             color[0] = m_colorCurve->GetCurveData(0)->GetKey(i).Value;
             color[1] = m_colorCurve->GetCurveData(1)->GetKey(i).Value;
             color[2] = m_colorCurve->GetCurveData(2)->GetKey(i).Value;
